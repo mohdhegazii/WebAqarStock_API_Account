8aeb4c8def2ceefd94fe2234d799bff11967bb6d
mohdhegazii <106732915+mohdhegazii@users.noreply.github.com>
2022-07-29 14:23:06 +0200

    Add files via upload

---
+++ WebAqarStock_API/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebAqarStock_API.Startup))]
+
+namespace WebAqarStock_API
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
